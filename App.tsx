3f809c8e01e1615aee15cec613b7654fbfb52879
87brandonn <53943296+87brandonn@users.noreply.github.com>
2022-02-22 21:57:51 +0700

    fix script

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hey!</Text>
+      <Text>Checking</Text>
       <StatusBar style="auto" />
     </View>
   );
