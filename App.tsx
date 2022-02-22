03c552ea9f339f000b7c7319009f52f3ade1455c
87brandonn <53943296+87brandonn@users.noreply.github.com>
2022-02-22 22:18:24 +0700

    add twrnc

--- App.tsx
+++ App.tsx
@@ -1,10 +1,15 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import tw from 'twrnc';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hi</Text>
+      <View style={tw`bg-red-500 flex flex-row justify-center rounded-lg py-2 px-5`}>
+        <View>
+          <Text style={tw`text-red-200 text-3xl`}>Catetin</Text>
+        </View>
+      </View>
       <StatusBar style="auto" />
     </View>
   );
